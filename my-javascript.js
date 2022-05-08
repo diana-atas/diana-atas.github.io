a4d7837fe71af43e9fd88c725fb308b219d8c335
diana-atas <dianadavidatas@gmail.com>
2022-05-09 00:22:15 +1200

    create my-javascript.js

---
+++ my-javascript.js
@@ -0,0 +1,26 @@
+let mainNavLinks = document.querySelectorAll('nav ul li a')
+let mainSections = document.querySelectorAll('main section')
+
+let lastId
+let cur = []
+
+window.addEventListener('scroll', () => {
+  _.throttle(doThatStuff, 100)
+})
+
+window.addEventListener('scroll', (event) => {
+  let fromTop = window.scrollY
+
+  mainNavLinks.forEach((link) => {
+    let section = document.querySelector(link.hash)
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add('current')
+    } else {
+      link.classList.remove('current')
+    }
+  })
+})
